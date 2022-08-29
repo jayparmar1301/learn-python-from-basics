55f8bf01b8cab76715d7e9f1519044841a4ad1ba
jay <jay@vebuin.com>
2022-08-29 16:12:58 +0530

    [Modified] methods parenthesis, added remove and extend method

--- basics/list.py
+++ basics/list.py
@@ -5,33 +5,38 @@
 simple_list_with_values = [1,3,4,5,6,3,4,4]
 
 # list has variety range of in build functions which we can use
-# sort method will sort all element in acending order
+# sort() method will sort all element in acending order
 simple_list_with_values.sort()
 
-# append method is used to add new elements in existing list
+# append() method is used to add new elements in existing list
 simple_list_with_values.append(33)
 
-# copy method used for copying main list as copy to new list 
+# copy() method used for copying main list as copy to new list
 simple_list_with_values.copy()
 
-# count method will gives us num of occurrence that particular element has
+# count() method will gives us num of occurrence that particular element has
 # let say simple_list_with_values list, occurrence of 4 is 3 times so it will return 3 
 simple_list_with_values.count(4)
 
-
-# index method will return index of given element
+# index() method will return index of given element
 # note: if same element occurred 2 times in list then it will only return first index
 simple_list_with_values.index(4)
 
-
-# pop method will remove last element from list
+# pop() method will remove last element from list
 simple_list_with_values.pop()
 
-# reverse method will reverse list
+# remove() method will remove particular element from list
+simple_list_with_values.remove(4)
+
+# reverse() method will reverse list
 # [1,3,4,5,6,3,4,4] -> [4,4,3,6,5,4,3,1]
 simple_list_with_values.reverse()
 
-# clear method will remove all elements from list 
+# extend() method used to string one-by-one into list
+# ("123") -> [1,2,3] in existing list
+simple_list_with_values.extend("123")
+
+# clear() method will remove all elements from list
 simple_list_with_values.clear()
 
 ## list with different data type
